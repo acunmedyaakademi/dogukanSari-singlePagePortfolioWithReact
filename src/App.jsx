8cabb759baa1f77638b6deb438c281dbf3b73b81
Doğukan Sarı <teddogukan@gmail.com>
2024-08-07 12:51:35 +0300

    Images klasörünün ismi images olarak değiştirildi. App.jsx içindeki html yapıları düzenlendi.

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 function Header() {
   return (
     <div className="header">
-      <a href="#"><img src="../public/Images/mobilelogo.svg" alt="Logo" /></a>
+      <a href="#"><img src="/images/mobilelogo.svg" alt="Logo" /></a>
       <a href="#" className="header-btn">Free Consultation</a>
   </div>
   )  
@@ -37,12 +37,12 @@ function Hero() {
 function ContainerContent() {
   return(
     <div className="content-container">
-      <div className="content"><img src="../public/Images/graphicdeign-img.png"/>Graphic Design</div>
-      <div className="content tek"><img src="../public/Images/uiux-img.png"/>UI/UX</div>
-      <div className="content tek"><img src="../public/Images/apps-img.png"/>Apps</div>
-      <div className="content"><img src="../public/Images/Illustrations-img.png"/>Illustrations</div>
-      <div className="content"><img src="../public/Images/photography-img.png"/>Photography</div>
-      <div className="content"><img src="../public/Images/motiongraphic-img.png"/>Motion Graphics</div>
+      <div className="content"><img src="/images/graphicdeign-img.png"/>Graphic Design</div>
+      <div className="content tek"><img src="/images/uiux-img.png"/>UI/UX</div>
+      <div className="content tek"><img src="/images/apps-img.png"/>Apps</div>
+      <div className="content"><img src="/images/Illustrations-img.png"/>Illustrations</div>
+      <div className="content"><img src="/images/photography-img.png"/>Photography</div>
+      <div className="content"><img src="/images/motiongraphic-img.png"/>Motion Graphics</div>
     </div>
   )
 }
@@ -50,7 +50,7 @@ function ContainerContent() {
 function AboutMe() {
   return(
     <div className="about-me">
-      <img src="../public/Images/aboutme-img.png" alt="Amy" />
+      <img src="/images/aboutme-img.png" alt="Amy" />
       <div className="about-me-description">
         <h2>I’m Amy, and I’d love to work on your next project</h2>
         <p>I love working with others to create beautiful design solutions. I’ve designed everything from brand illustrations to complete mobile apps. I’m also handy with a camera!</p>
@@ -75,7 +75,7 @@ function Booking() {
 function Footer() {
   return(
     <div className="footer">
-      <a href="/index.html"><img src="../public/Images/mobilelogo.svg" alt="Logo" /></a>
+      <a href="/index.html"><img src="/images/mobilelogo.svg" alt="Logo" /></a>
       <a href="#" className="header-btn">Free Consultation</a>
     </div>
   )
